0a084ebae9edfd3e8481e03cf9d605336a7fae49
asanka9 <sendasanka96@gmail.com>
2020-11-27 21:54:39 +0530

    sdsd

--- src/app/services/data-base.service.ts
+++ src/app/services/data-base.service.ts
@@ -7,4 +7,60 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class DataBaseService {
 
   constructor( private firestore: AngularFirestore) { }
+
+
+  //Apps Data Base
+  createNewApp(data) {
+    return new Promise<any>((resolve, reject) =>{
+        this.firestore
+            .collection("Apps")
+            .add(data)
+            .then(res => {}, err => reject(err));
+    });
+  }
+
+  getAllApps(){
+    return this.firestore.collection("Apps").snapshotChanges();
+  }
+
+  deleteApp(data) {
+    return this.firestore.collection("Apps").doc(data.payload.doc.id).delete();
+ }
+
+ //Category Data Base
+ createNewCategory(data,appname) {
+  return new Promise<any>((resolve, reject) =>{
+      this.firestore
+          .collection(appname)
+          .add(data)
+          .then(res => {}, err => reject(err));
+  });
+}
+
+getAllCategories(appname){
+  return this.firestore.collection(appname).snapshotChanges();
+}
+
+deleteCategory(data,appname) {
+  return this.firestore.collection(appname).doc(data.payload.doc.id).delete();
+}
+
+//Item Data Base
+createNewItem(data,appname,categoryname) {
+  return new Promise<any>((resolve, reject) =>{
+      this.firestore
+          .collection(appname+categoryname)
+          .add(data)
+          .then(res => {}, err => reject(err));
+  });
+}
+
+getAllItem(appname,categoryname){
+  return this.firestore.collection(appname+categoryname).snapshotChanges();
+}
+
+deletItem(data,appname,categoryname) {
+  return this.firestore.collection(appname+categoryname).doc(data.payload.doc.id).delete();
+}
+
 }
--- src/app/shared/steppers/appss/appss.component.ts
+++ src/app/shared/steppers/appss/appss.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataBaseService} from 'src/app/services/data-base.service';
 
 @Component({
   selector: 'app-appss',
@@ -8,14 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class AppssComponent implements OnInit {
 
   youtubeList = [1,2,3,34,4]
+  appList = []
 
-  constructor() { }
+  type:string;
+
+  constructor(private database : DataBaseService) { }
 
   ngOnInit(): void {
+     this.database.getAllApps().subscribe(
+      res=>  (this.appList = res)
+    );
   }
 
-  onBookChange(event){
-    alert(event.value);
-
+  addApp(){
+    this.database.createNewApp(
+      {'name':2121}
+    );
   }
+
+  deleteApp(data){
+    //this.database.deleteApp(data);
+    this.database.createNewItem({'name':2121},'_a','_b');
+  }
+
+
 }
