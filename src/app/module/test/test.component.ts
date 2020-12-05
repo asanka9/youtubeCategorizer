b44540e0dd08e92ca10a56d4774ceb67cf3e75cd
asanka9 <sendasanka96@gmail.com>
2020-12-05 19:52:51 +0530

    dsd

--- src/app/module/test/test.component.ts
+++ src/app/module/test/test.component.ts
@@ -9,7 +9,13 @@ export class TestComponent implements OnInit {
 
   temp = [23,32,3,23,232]
 
-  htmlList = []
+  htmlList = [''];
+  selectedTypeList = [''];
+  selectedType : string;
+  finalIndex = 0;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,15 +25,51 @@ export class TestComponent implements OnInit {
     alert(num);
   }
 
-  saveButtonClick(index){
+  getCssClass(value){
+    switch (value) {
+      case value:
+
+        break;
+
+      default:
+        break;
+    }
   }
 
-  deleteButtonClick(index){
+  saveButtonClick(value,index){
+    this.htmlList[index] = value;
+  }
 
+  deleteButtonClick(index,name){
+    this.htmlList.splice(-1,1);
+    this.selectedTypeList.splice(-1,1);
+    this.finalIndex --;
   }
 
   createNewButtonClick(index){
+    this.htmlList.reverse();
+    this.htmlList.push('');
+    this.htmlList.reverse();
+    this.selectedTypeList.reverse();
+    this.selectedTypeList.push('');
+    this.selectedTypeList.reverse();
+    this.finalIndex ++;
 
+  }
+
+  selectType(event,num){
+    event.preventDefault();
+    this.selectedTypeList[num] = event.value;
+  }
+
+  genarateFinalString(){
+    let ouput = '';
+    for (let index = 0; index < this.selectedTypeList.length; index++) {
+      let type = this.selectedTypeList[index];
+      let value = this.htmlList[index];
+      ouput+= type + value;
+    }
+    alert(ouput);
   }
 
 }
